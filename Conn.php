a4e646f4283d43a4780e3a217b08782b3e7ed34d
NH-X <1709597119@qq.com>
2023-06-22 01:48:47 +0800

    2023/06/22 01:48

---
+++ Conn.php
@@ -0,0 +1,38 @@
+<?php
+    class Conn extends mysqli{
+        private static $instance;
+        private static $servername;
+        private static $port;
+        private static $dbUser;
+        private static $dbPassword;
+        private static $dbName;
+
+        private function __construct() {
+            // 私有构造方法，防止直接实例化
+            // 读取配置文件
+            $configFile = file_get_contents("config.json");
+            $config = json_decode($configFile, true);
+
+            // 从配置文件中获取数据库连接信息
+            self::$servername = $config["servername"];
+            self::$port = $config["port"];
+            self::$dbUser = $config["dbUser"];
+            self::$dbPassword = $config["dbPassword"];
+            self::$dbName = $config["dbName"];
+
+            parent::__construct(
+                self::$servername,
+                self::$dbUser,
+                self::$dbPassword,
+                self::$dbName
+            );
+        }
+
+        public static function getInstance() {
+            if (self::$instance === null) {
+                self::$instance = new self();
+            }
+            return self::$instance;
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,82 +1,101 @@
-﻿<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>个人博客首页面</title>
-<link href="style/style.css" rel="stylesheet" type="text/css">
-</head>
+﻿<?php
+    require_once 'Conn.php';
+    $conn=Conn::getInstance();
+?>
 
-<body>
-<div id="box">
-  <div id="left-bar">
-    <div id="quickmenu"><img src="images/work1.gif" width="88" height="70" alt=""><img src="images/work2.gif" width="88" height="70" alt=""><img src="images/work3.gif" width="88" height="70" alt=""><img src="images/work4.gif" width="88" height="70" alt=""><img src="images/work5.gif" width="88" height="70" alt=""><img src="images/left_btn.gif" width="105" height="22" style="margin-top:25px;" alt=""></div>
-  </div>
-  <div id="main-bg">
-    <div id="top">
-      <div id="logo"><img src="images/logo.jpg" width="223" height="47" alt=""></div>
-      <div id="menu">
-        <ul>
-          <li>博客</li>
-          <li>相册</li>
-          <li>案例</li>
-          <li>服务</li>
-          <li>博友</li>
-          <li>留言</li>
-          <li>联系</li>
-        </ul>
-      </div>
-    </div>
-    <div id="banner"><img src="images/banner.jpg" width="808" height="343" alt=""></div>
-    <div id="pic-bg">
-      <div id="pic-left"><img src="images/work_left.gif" width="27" height="57" alt=""></div>
-      <div class="pic01"><img src="images/work6.gif" width="112" height="102" alt=""><br>
-        开心大眼仔</div>
-      <div class="pic01"><img src="images/work7.gif" width="112" height="102" alt=""><br>
-        绝望主妇</div>
-      <div class="pic01"><img src="images/work8.gif" width="112" height="102" alt=""><br>
-        精美图标集</div>
-      <div class="pic01"><img src="images/work9.gif" width="112" height="102" alt=""><br>
-        机器狗阿七</div>
-      <div id="pic-right"><img src="images/work_right.gif" width="27" height="57" alt=""></div>
-    </div>
-    <div id="main">
-      <div id="left">
-        <div id="content-bg">
-          <div id="content-top">
-            <div id="content-title"><img src="images/index11.gif" width="19" height="20" alt="">这里是博客标题</div>
-            <div id="content-tian"><img src="images/weather_cloudy.gif" width="16" height="16" alt=""></div>
-            <div id="content-date">这里是日期</div>
-          </div>
-          <div id="content-text"><img src="images/index06.gif" width="550" height="80" alt=""><br>
-          798之行，周五和公司同事一起去了798，幻想着也许在里面能够追寻一些有用的灵感，也许这也是一个新的开始吧！
-          </div>
-          <div id="content-more"><a href="#" target="_blank"><img src="images/index07.gif" width="13" height="10" alt=""> 查看详情</a></div>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>个人博客首页面</title>
+        <link href="style/style.css" rel="stylesheet" type="text/css">
+    </head>
+
+    <body>
+        <div id="box">
+            <div id="left-bar">
+                <div id="quickmenu"><img src="images/work1.gif" width="88" height="70" alt=""><img src="images/work2.gif" width="88" height="70" alt=""><img src="images/work3.gif" width="88" height="70" alt=""><img src="images/work4.gif" width="88" height="70" alt=""><img src="images/work5.gif" width="88" height="70" alt=""><img src="images/left_btn.gif" width="105" height="22" style="margin-top:25px;" alt=""></div>
+            </div>
+            <div id="main-bg">
+                <div id="top">
+                    <div id="logo"><img src="images/logo.jpg" width="223" height="47" alt=""></div>
+                    <div id="menu">
+                        <ul>
+                            <li>博客</li>
+                            <li>相册</li>
+                            <li>案例</li>
+                            <li>服务</li>
+                            <li>博友</li>
+                            <li>留言</li>
+                            <li>联系</li>
+                        </ul>
+                    </div>
+                </div>
+                <div id="banner"><img src="images/banner.jpg" width="808" height="343" alt=""></div>
+                <div id="pic-bg">
+                    <div id="pic-left"><img src="images/work_left.gif" width="27" height="57" alt=""></div>
+                    <div class="pic01"><img src="images/work6.gif" width="112" height="102" alt=""><br>
+                      开心大眼仔</div>
+                    <div class="pic01"><img src="images/work7.gif" width="112" height="102" alt=""><br>
+                      绝望主妇</div>
+                    <div class="pic01"><img src="images/work8.gif" width="112" height="102" alt=""><br>
+                      精美图标集</div>
+                    <div class="pic01"><img src="images/work9.gif" width="112" height="102" alt=""><br>
+                      机器狗阿七</div>
+                    <div id="pic-right"><img src="images/work_right.gif" width="27" height="57" alt=""></div>
+                </div>
+                <div id="main">
+                    <div id="left">
+                        <div id="content-bg">
+                            <div id="content-top">
+                                <div id="content-title"><img src="images/index11.gif" width="19" height="20" alt="">这里是博客标题</div>
+                                <div id="content-tian"><img src="images/weather_cloudy.gif" width="16" height="16" alt=""></div>
+                                <div id="content-date">这里是日期</div>
+                            </div>
+                        <div id="content-text">
+                            <img src="images/index06.gif" width="550" height="80" alt="">
+                            <br>
+                          798之行，周五和公司同事一起去了798，幻想着也许在里面能够追寻一些有用的灵感，也许这也是一个新的开始吧！
+                        </div>
+                            <div id="content-more">
+                                <a href="#" target="_blank">
+                                    <img src="images/index07.gif" width="13" height="10" alt=""> 查看详情
+                                </a>
+                            </div>
+                        </div>
+                        <div id="no-content">暂时还没有日志，赶快写日志吧！</div>
+                        <div id="content-bar">第一页　　上一页　　下一页　　最后一页</div>
+                    </div>
+                    <div id="right">
+                        <div id="right-menu">
+                            <ul>
+                                <li><img src="images/index11.gif" width="19" height="20" alt="">日志 Blogs</li>
+                                <li><img src="images/index12.gif" width="17" height="18" alt="">文章 Aticle</li>
+                                <li><img src="images/index13.gif" width="19" height="19" alt="">作品 Works</li>
+                                <li><img src="images/index14.gif" width="20" height="17" alt="">图片 Photos</li>
+                                <li><img src="images/index15.gif" width="21" height="18" alt="">其他 Others</li>
+                                <li><img src="images/index16.gif" width="19" height="17" alt="">下载 Download</li>
+                                <li><img src="images/index17.gif" width="21" height="19" alt="">留言 Guestbook</li>
+                            </ul>
+                        </div>
+                        <div id="right-gltitle">
+                            <img src="images/index18.gif" width="81" height="19" alt="">
+                        </div>
+                        <div id="right-gltext">
+                            <ul>
+                                <li>
+                                    <a href="./admin/login.php" class="link01">
+                                        管理登录
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div id="bottom">
+                    Kiss2018 Design 2018©all rights reserved    |   京ICP备00000000号
+                </div>
+            </div>
         </div>
-        <div id="no-content">暂时还没有日志，赶快写日志吧！</div>
-        <div id="content-bar">第一页　　上一页　　下一页　　最后一页</div>
-      </div>
-      <div id="right">
-        <div id="right-menu">
-          <ul>
-            <li><img src="images/index11.gif" width="19" height="20" alt="">日志 Blogs</li>
-            <li><img src="images/index12.gif" width="17" height="18" alt="">文章 Aticle</li>
-            <li><img src="images/index13.gif" width="19" height="19" alt="">作品 Works</li>
-            <li><img src="images/index14.gif" width="20" height="17" alt="">图片 Photos</li>
-            <li><img src="images/index15.gif" width="21" height="18" alt="">其他 Others</li>
-            <li><img src="images/index16.gif" width="19" height="17" alt="">下载 Download</li>
-            <li><img src="images/index17.gif" width="21" height="19" alt="">留言 Guestbook</li>
-          </ul>
-        </div>
-        <div id="right-gltitle"><img src="images/index18.gif" width="81" height="19" alt=""></div>
-        <div id="right-gltext">
-          <ul>
-            <li><a href="#" class="link01">管理登录</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div id="bottom">Kiss2018 Design 2018©all rights reserved    |   京ICP备00000000号</div>
-  </div>
-</div>
-</body>
+    </body>
 </html>
